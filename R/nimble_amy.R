9db513b832756ac8fcf4ce96515278f4bf05976e
John Foster <john.foster@usda.gov>
2023-01-11 16:12:31 -0700

    control flow for different model structures

--- R/nimble_amy.R
+++ R/nimble_amy.R
@@ -1,6 +1,25 @@
 modelCode <- nimbleCode({
 
+  ### Priors
+  # capture probability by method
+  for(k in 1:n_methods){
+    mu_p[k] ~ dnorm(0, tau = 1)
+    logit(p[k]) <- mu_p[k]
+  }
+
+  # process error
+  if(include_proc){
+    tau_p ~ dexp(1)
+  }
+
   for(i in 1:n_property){
+
+    # growth rate coefficient priors
+    if(include_beta){
+      for(j in 1:n_beta){
+        beta[i, j] ~ dnorm(0, tau = 1)
+      }
+    }
 
     n[i, 1] ~ dpois(mu1[i])
     log(mu1[i]) <- log_mu1[i]
@@ -8,28 +27,36 @@ modelCode <- nimbleCode({
 
     for(t in 2:n_timestep[i]){
 
-      # lambda is growth rate (exponential population growth)
-      # log(lambda[i]) ~ dnorm(X %*% beta, sigma_l)
-      # log(lambda[i, t]) <- lambda_mu[i, t]
-      log_lambda[i, t-1] ~ dnorm(0, tau = 0.1)
-
       n[i, t] ~ dpois(mu[i, t])
-      log(mu[i, t]) <- log_mu_proc[i, t]
-      log_mu_proc[i, t] ~ dnorm(log_mu[i, t], tau = tau_p)
+
+      if(include_proc){
+        log(mu[i, t]) <- log_mu_proc[i, t]
+        log_mu_proc[i, t] ~ dnorm(log_mu[i, t], tau = tau_p)
+      } else {
+        log(mu[i, t]) <- log_mu[i, t]
+      }
+
       log_mu[i, t] <- log_lambda[i, t-1] + log(z[i, t-1])
       z[i, t-1] <- n[i, t-1] - y_removed[i, t-1]
+
     }
   }
 
-  tau_p ~ dexp(1)
-
   for(i in 1:n_units){
+
+    # lambda is growth rate (exponential population growth)
+    if(include_beta){
+      # currently using basis functions that vary by property and time
+      log_lambda[property[i], timestep[i]] <- inprod(X[i, 1:n_beta], beta[property[i], 1:n_beta])
+    } else {
+      log_lambda[property[i], timestep[i]] ~ dnorm(0, tau = 1)
+    }
 
     # poisson likelihood - first pass
     y[i, 1] ~ dpois(pi[i, 1] * n[property[i], timestep[i]])
-    pi[i, 1] <- gamma[i, 1] * theta[i, 1]
+    pi[i, 1] <- gamma[i, 1] * theta[i, 1] *
+      (1 - gamma[i, 1]) + (gamma[i, 1] * (1 - theta[i, 1]))
     theta[i, 1] <- 1 - (1 - p[method[i, 1]]) ^ effort[i, 1]
-
 
     for(j in 2:n_passes[i]){
 
@@ -42,14 +69,7 @@ modelCode <- nimbleCode({
     }
   }
 
-  for(k in 1:n_methods){
-    mu_p[k] ~ dnorm(0, tau = 1)
-    logit(p[k]) <- mu_p[k]
-  }
 
-  # for(i in 1:n_beta){
-  #   beta[i] ~ dnorm(0, 1)
-  # }
 
 })
 
