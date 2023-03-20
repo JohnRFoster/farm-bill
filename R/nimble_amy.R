f14aa917fd980fd46ef37b4e9fa7889427df8dca
John Foster <john.foster@usda.gov>
2023-03-20 17:22:41 -0600

    control flow for simple model simulations

--- R/nimble_amy.R
+++ R/nimble_amy.R
@@ -3,10 +3,12 @@ modelCode <- nimbleCode({
   ### Priors
   # capture probability by method
   for(k in 1:n_methods){
-    for(t in 1:nt){
-      mu_p[k, t] ~ dnorm(0, tau = 1)
-      logit(p[k, t]) <- mu_p[k, t]
-    }
+    # for(t in 1:nt){
+    #   mu_p[k, t] ~ dnorm(0, tau = 1)
+    #   logit(p[k, t]) <- mu_p[k, t]
+    # }
+      mu_p[k] ~ dnorm(0, tau = 1)
+      logit(p[k]) <- mu_p[k]
   }
 
   # if(include_county){
@@ -15,6 +17,10 @@ modelCode <- nimbleCode({
   #     logit(r[i]) <- mu_k[i]
   #   }
   # }
+
+  if(!include_beta){
+    mean_r ~ dnorm(0, tau = 1)
+  }
 
   # process error
   if(include_proc){
@@ -31,8 +37,9 @@ modelCode <- nimbleCode({
       }
     }
 
-    n[i, 1] ~ dpois(mu1[i])
-    log(mu1[i]) <- log_mu1[i]
+    n[i, 1] ~ dpois(mu[i, 1])
+    log(mu[i, 1]) <- log_mu_proc[i, 1]
+    log_mu_proc[i, 1] <- log_mu1[i]
     log_mu1[i] ~ dnorm(0, tau = 0.1)
 
     for(t in 2:n_timestep[i]){
@@ -49,7 +56,7 @@ modelCode <- nimbleCode({
       z[i, t-1] <- n[i, t-1] - y_removed[i, t-1]
 
       if(include_all_pp){
-        log_lambda[i, t-1] <- sum(log_lambda_all[i, (pp[i, timestep[t-1]]):(pp[i, timestep[t]])])
+        log_lambda[i, t-1] <- sum(log_lambda_all[i, pp[i, t-1]:pp[i, t]])
       }
     }
   }
@@ -57,7 +64,7 @@ modelCode <- nimbleCode({
   if(include_all_pp){
     for(i in 1:n_units_all){
       if(intercept_only){
-        log_lambda_all[property_x[i], timestep_x[i]] <- beta[property_x[i], 1]
+        log_lambda_all[property_x[i], timestep_x[i]] <- mean_r
       } else {
         log_lambda_all[property_x[i], timestep_x[i]] <- inprod(Xall[i, 1:n_beta], beta[property_x[i], 1:n_beta])
       }
@@ -67,7 +74,7 @@ modelCode <- nimbleCode({
   for(i in 1:n_units){
 
     # lambda is growth rate (exponential population growth)
-    if(not_include_all_pp){
+    if(!include_all_pp){
       if(include_beta){
         # currently using basis functions that vary by property and time
         log_lambda[property[i], timestep[i]] <- inprod(X[i, 1:n_beta], beta[property[i], 1:n_beta])
@@ -76,34 +83,50 @@ modelCode <- nimbleCode({
       }
     }
 
-    if(include_county){
-      Nc[countyN[i], cnty_property[i], PPNum[i]] <- n[property[i], timestep[i]]
+    Nc[countyN[i], cnty_property[i], PPNum[i]] <- n[property[i], timestep[i]]
+
+    # likelihood - first pass
+    if(nb_likelihood){
+      y[i, 1] ~ dnegbin(py[i, 1], size[countyN[i]])
+      py[i, 1] <- size[countyN[i]] / (y_mu[i, 1] + size[countyN[i]])
+      y_mu[i, 1] <- pi[i, 1] * n[property[i], timestep[i]]
     }
 
-    # poisson likelihood - first pass
-    y[i, 1] ~ dpois(pi[i, 1] * n[property[i], timestep[i]])
+    if(pois_likelihood){
+      y[i, 1] ~ dpois(pi[i, 1] * n[property[i], timestep[i]])
+    }
+
     pi[i, 1] <- gamma[i, 1] * theta[i, 1] * (1 - gamma[i, 1]) + (gamma[i, 1] * (1 - theta[i, 1]))
-    theta[i, 1] <- 1 - (1 - p[method[i, 1], timestep[i]]) ^ effort[i, 1]
+    theta[i, 1] <- 1 - (1 - p[method[i, 1]]) ^ effort[i, 1]
 
     for(j in 2:n_passes[i]){
 
-      # poisson likelihood - subsequent passes
-      y[i, j] ~ dpois(pi[i, j] * n[property[i], timestep[i]])
+      # likelihood - subsequent passes
+      if(nb_likelihood){
+        y[i, j] ~ dnegbin(py[i, j], size[countyN[i]])
+        py[i, j] <- size[countyN[i]] / (y_mu[i, j] + size[countyN[i]])
+        y_mu[i, j] <- pi[i, j] * n[property[i], timestep[i]]
+      }
+
+      if(pois_likelihood){
+        y[i, j] ~ dpois(pi[i, j] * n[property[i], timestep[i]])
+      }
+
       pi[i, j] <- gamma[i, j] * theta[i, j] *
         exp(sum(log((1 - gamma[i, 1:(j-1)]) + (gamma[i, 1:(j-1)] * (1 - theta[i, 1:(j-1)])))))
-      theta[i, j] <- 1 - (1 - p[method[i, j], timestep[i]]) ^ effort[i, j]
+      theta[i, j] <- 1 - (1 - p[method[i, j]]) ^ effort[i, j]
 
     }
 
   }
 
-  beta_k[1:4] ~ dmnorm(k_mu[1:4], k_cov[1:4, 1:4])
+  # beta_k[1:4] ~ dmnorm(k_mu[1:4], k_cov[1:4, 1:4])
 
   for(i in 1:n_county){
 
-    # size[i] ~ dunif(0, 50) # property dispersion
+    size[i] ~ dunif(0, 50) # property dispersion
     # size_m[i] <- dexp(1) # county dispersion
-    log(size[i]) <- inprod(beta_k[1:4], Xk[i, 1:4])
+    # log(size[i]) <- inprod(beta_k[1:4], Xk[i, 1:4])
   }
 
 
@@ -111,10 +134,10 @@ modelCode <- nimbleCode({
 
     ### property dispersion ###
     # total abundance in sampled properties within county i
-    N_disp[i] ~ dnegbin(pN[i], size[county[i]])
+    # N_disp[i] ~ dnegbin(pN[i], size[county[i]])
 
     # re-parametrization with mean abundance
-    pN[i] <- size[county[i]] / (N_mu[i] + size[county[i]])
+    # pN[i] <- size[county[i]] / (N_mu[i] + size[county[i]])
 
     # all pigs in county i at time j
     # N_mu[i, j] <- sum(Nc[i, 1:n_prop_cnty[i], pp_c[i, j]])
@@ -130,7 +153,7 @@ modelCode <- nimbleCode({
     pM[i] <- size[county[i]] / (M_mu[i] + size[county[i]])
 
     # convert abundance to density (across properties), scale to county abundance
-    M_mu[i] <- N_disp[i] / sum_prop_area[i] * county_area[county[i]]
+    M_mu[i] <- N_mu[i] / sum_prop_area[i] * county_area[county[i]]
 
 
   }
