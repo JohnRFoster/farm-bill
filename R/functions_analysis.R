aa5b088c1a1edb8607a865ae0cdded5ec917ca0b
JohnRFoster <fosterj@bu.edu>
2023-07-12 14:03:57 -0600

    more figures

--- R/functions_analysis.R
+++ R/functions_analysis.R
@@ -23,7 +23,7 @@ read_output <- function(filedest) {
 ############
 # modify output with correct names, time steps, and properties
 ############
-name_nodes <- function(mcmc_samples, model_name, n_mcmc = 5000){
+name_nodes <- function(mcmc_samples, n_nodes, m_nodes, model_name, dm_lookup, n_mcmc = 5000){
 
   method_lookup <- method_lookup |>
     rename(method = Methods)
@@ -34,34 +34,74 @@ name_nodes <- function(mcmc_samples, model_name, n_mcmc = 5000){
   mcmc <- samples_mat |>
     as_tibble() |>
     slice(draws) |>
-    select(contains(nodes1), all_of(nodes2)) |>
+    select(contains(nodes1), all_of(n_nodes), all_of(m_nodes)) |>
     mutate(iter = 1:n()) |>
     pivot_longer(cols = -iter,
                  names_to = "node") |> #filter(grepl("tau_p", node))
     mutate(
       model = model_name,
-      name = if_else(grepl("mu_p", node), "Capture Prob", "x"),
-      name = if_else(grepl("log_mu1", node), "Initial Condition", name),
-      name = if_else(grepl("lambda", node), "Growth Rate", name),
+      name = if_else(grepl("mu_p[", node, fixed = TRUE), "Capture Prob", "x"),
+      name = if_else(grepl("z1", node), "Initial Condition", name),
+      name = if_else(grepl("mu_phi", node), "Survival Rate", name),
+      name = if_else(grepl("log_rho", node), "Per Capita Recruitment Rate", name),
       name = if_else(grepl("n", node), "Abundance", name),
       name = if_else(grepl("M", node), "County Abundance", name),
-      name = if_else(grepl("N_disp", node), "Property Abundance with dispersion", name),
+      name = if_else(grepl("log_mean_r", node), "Growth Rate", name),
+      # name = if_else(grepl("N_disp", node), "Property Abundance with dispersion", name),
       name = if_else(grepl("tau", node), "Process Error", name),
       name = if_else(grepl("beta", node), "Basis Function Coefficients", name),
-      value = if_else(grepl("mu_p", node), boot::inv.logit(value), value),
-      value = if_else(grepl("log_mu1", node), exp(value), value),
-      value = if_else(grepl("lambda", node), exp(value), value),
-      # value = if_else(name == "Process Error", 1/sqrt(value), value),
-      property_idx = as.numeric(str_extract(node, "(?<=\\[)\\d")),
-      property_idx = as.numeric(property_idx),
-      property_idx = if_else(grepl("mu_p", node), -1, property_idx),
-      timestep = as.numeric(str_extract(node, "\\d*(?=\\])")),
-      timestep = as.numeric(timestep),
-      method_idx = as.numeric(str_extract(node, "(?<=mu_p\\[)\\d")),
-      ) |>
+      value = if_else(grepl("mu_p[", node, fixed = TRUE), boot::inv.logit(value), value),
+      value = if_else(grepl("mu_phi", node), boot::inv.logit(value), value),
+      value = if_else(grepl("log_rho", node), exp(value), value),
+      value = if_else(grepl("log_mean_r", node), exp(value), value))
+
+  my_summary <- function(df){
+   df |> summarise(lower = quantile(value, 0.025),
+                   median = quantile(value, 0.5),
+                   upper = quantile(value, 0.975))
+  }
+
+  n_mcmc <- mcmc |>
+    filter(grepl("n[", node, fixed = TRUE)) |>
+    mutate(property_idx = as.numeric(str_extract(node, "(?<=\\[)\\d")),
+           timestep = as.numeric(str_extract(node, "\\d*(?=\\])"))) |>
+    left_join(property_lookup) |>
+    group_by(node, model, name, property_idx, timestep, Property, PPNum, pp_start_date, pp_end_date) |>
+    my_summary() |>
+    ungroup()
+
+  m_mcmc <- mcmc |>
+    filter(grepl("M[", node, fixed = TRUE)) |>
+    mutate(m_idx = as.numeric(str_extract(node, "(?<=\\[)\\d*")),
+           value = log(value + 1)) |>
+    left_join(county_level_lookup, by = "m_idx") |>
+    group_by(node, model, name, m_idx, County, PPNum, sum_area) |>
+    my_summary() |>
+    ungroup()
+
+  p_mcmc <- mcmc |>
+    filter(grepl("mu_p[", node, fixed = TRUE)) |>
+    mutate(method_idx = as.numeric(str_extract(node, "(?<=mu_p\\[)\\d"))) |>
     left_join(method_lookup) |>
-    left_join(property_lookup)
-  return(mcmc)
+    group_by(node, model, name, method_idx, method) |>
+    my_summary() |>
+    ungroup()
+
+  dm_mcmc <- mcmc |>
+    filter(grepl("mu_phi", node, fixed = TRUE) |
+             grepl("log_rho", node, fixed = TRUE)) |>
+    mutate(property_timestep_idx = as.numeric(str_extract(node, "\\d*(?=\\])"))) |>
+    left_join(dm_lookup) |>
+    group_by(node, model, name, property_timestep_idx, Property, property_idx, PPNum, pp_start_date, pp_end_date, timestep) |>
+    my_summary() |>
+    ungroup()
+
+  z1_mcmc <- mcmc |> filter(node == "z1")  |>
+    group_by(node, model, name) |>
+    my_summary() |>
+    ungroup()
+
+  return(bind_rows(n_mcmc, m_mcmc, p_mcmc, dm_mcmc, z1_mcmc))
 }
 
 
@@ -114,6 +154,25 @@ gg_lambda_pointrange <- function(mcmc_quants){
 }
 
 ############
+# plot survival
+############
+gg_lambda_pointrange <- function(mcmc_quants){
+  mcmc_quants |>
+    filter(name == "Survival Rate") |>
+    ggplot() +
+    aes(x = pp_end_date, ymin = lower, ymax = upper, y = median, color = model) +
+    geom_pointrange(position = position_dodge(width=45)) +
+    geom_hline(yintercept = 1, linetype = "dashed") +
+    facet_wrap(~ Property) +
+    # coord_cartesian(ylim = c(0, 15)) +
+    labs(title = "Survival Rate",
+         x = "PP end date",
+         y = "Growth rate") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+}
+
+############
 # plot abundance
 ############
 gg_abundance_pointrange <- function(mcmc_quants, removed){
@@ -130,14 +189,54 @@ gg_abundance_pointrange <- function(mcmc_quants, removed){
     filter(name == "Abundance") |>
     left_join(obs) |>
     ggplot() +
-    aes(x = pp_end_date, ymin = lower, ymax = upper, y = median, color = model) +
-    geom_pointrange(position = position_dodge(width=45)) +
+    aes(x = pp_end_date, ymin = lower, ymax = upper, y = median) +
+    geom_pointrange(position = position_dodge(width=45), color = "blue") +
+    geom_line(position = position_dodge(width=45), color = "blue") +
+    geom_line(aes(y = Observed), color = "black") +
     geom_point(aes(y = Observed), color = "black") +
-    facet_wrap(~ Property) +
+    facet_wrap( ~ model) +
     # scale_y_log10() +
     labs(title = "Abundnace",
          x = "PP end date",
          y = "Number of pigs") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+}
+
+
+############
+# plot county abundance
+############
+gg_county_abundance_pointrange <- function(mcmc_quants, county){
+  obs <- removed |>
+    as_tibble() |>
+    mutate(property_idx = 1:n()) |>
+    pivot_longer(cols = -property_idx,
+                 names_to = "timestep",
+                 values_to = "Observed") |>
+    mutate(timestep = as.numeric(timestep)) |>
+    left_join(property_lookup) |>
+    mutate(County = if_else(Property %in% c("Chilton", "Stratos", "Youman 1", "Corbin"), "Hampton", "x"),
+           County = if_else(Property %in% c("H&B", "McKinney"), "Newberry", County),
+           County = if_else(Property %in% c("Mixon", "Okeetee"), "Jasper", County)) |>
+    filter(!is.na(PPNum)) |>
+    group_by(County, PPNum) |>
+    summarise(observed = log(sum(Observed) + 1)) |>
+    filter(County == county)
+
+  mcmc_quants |>
+    filter(name == "County Abundance",
+           County == county) |>
+    left_join(obs) |>
+    ggplot() +
+    aes(x = PPNum, ymin = lower, ymax = upper, y = median) +
+    geom_pointrange(position = position_dodge(width=0.5), color = "blue") +
+    geom_line(aes(y = observed), color = "black") +
+    geom_point(aes(y = observed), color = "black") +
+    facet_wrap(~ model) +
+    # scale_y_log10() +
+    labs(y = "log(County Abundnace + 1)",
+         x = "PP") +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
 }
